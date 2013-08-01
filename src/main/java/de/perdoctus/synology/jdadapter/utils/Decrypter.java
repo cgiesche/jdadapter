3ced9d38660f710251f1aef42f5b33373208759e
Christoph Giesche <christoph.giesche@gmx.net>
2013-08-01 18:27:49 +0200

    Added GPLv3 license information.

--- src/main/java/de/perdoctus/synology/jdadapter/utils/Decrypter.java
+++ src/main/java/de/perdoctus/synology/jdadapter/utils/Decrypter.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2013 Christoph Giesche
+ *
+ * This file is part of jdadapter.
+ *
+ * jdadapter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jdadapter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jdadapter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.perdoctus.synology.jdadapter.utils;
 
 import java.io.IOException;
@@ -15,7 +34,7 @@ import sun.misc.BASE64Decoder;
 
 /**
  *
- * @author Christoph Giesche <christoph.giesche@gmx.net>
+ * @author Christoph Giesche
  */
 public class Decrypter {
     
